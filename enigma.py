271d8686ea0bfd76c1766e0628a37c40c1061a88
Faisal Fida <FA20-BCS-004@cuiwah.edu.pk>
2024-02-09 09:51:47 +0500

    first commit

---
+++ enigma.py
@@ -0,0 +1,38 @@
+import re
+
+class PIIFilter:
+    def __init__(self):
+        self.patterns = {}
+        self.anonymization_mapping = {}
+
+    def add_pattern(self, name, regex):
+        self.patterns[name] = re.compile(regex)
+
+    def add_anonymization_mapping(self, name, replacement):
+        self.anonymization_mapping[name] = replacement
+
+    def filter(self, text):
+        filtered_text = text
+        for name, pattern in self.patterns.items():
+            filtered_text = pattern.sub(f"<{name}>", filtered_text)
+            for match in pattern.finditer(text):
+                self.add_anonymization_mapping(name, match.group())
+        return filtered_text
+
+    def defilter(self, text):
+        defiltered_text = text
+        for name, replacement in self.anonymization_mapping.items():
+            defiltered_text = defiltered_text.replace(f"<{name}>",replacement)
+        return defiltered_text
+
+# Example Usage:
+pii_filter = PIIFilter()
+pii_filter.add_pattern("phone_number", r"(92\d{10}|03\d{9}|3\d{9})")
+
+text_with_pii = "John Doe's phone number is 03211234567."
+filtered_text = pii_filter.filter(text_with_pii)
+original_text = pii_filter.defilter(filtered_text)
+
+print(f"Original Text: {text_with_pii}")
+print(f"Filtered Text: {filtered_text}")
+print(f"Original Text: {original_text}")

ba898f2e84653f39d5a809a6cf822e517232dcee
Faisal Fida <FA20-BCS-004@cuiwah.edu.pk>
2024-02-09 10:16:06 +0500

    sa

--- enigma.py
+++ enigma.py
@@ -29,15 +29,16 @@ class EnigmaGuard:
 enigma_guard = EnigmaGuard()
 enigma_guard.add_pattern("phone_number", r"(92\d{10}|03\d{9}|3\d{9})")
 enigma_guard.add_pattern("email", r"([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})")
-enigma_guard.add_pattern("name", r"([A-Z][a-z]+ [A-Z][a-z]+)")
-enigma_guard.add_pattern("address", r"(\d+ [A-Z][a-z]+ [A-Z][a-z]+)")
-enigma_guard.addd_pattern("cnic", r"(\d{5}-\d{7}-\d{1})")
+enigma_guard.add_pattern("name", r"(Name:|Name is|My name is) ([a-zA-Z]+ [a-zA-Z]+)")
+enigma_guard.add_pattern("cnic", r"(CNIC:|CNIC is|My CNIC is) (\d{5}-\d{7}-\d)")
+enigma_guard.add_pattern("address", r"(Address:|Address is|My address is) (House \d+, Street \d+, Area \d+, City \d+)")
+enigma_guard.add_pattern("passport", r"(Passport:|Passport is|My passport is) ([A-Z]{2}\d{7})")
+enigma_guard.add_pattern("credit_card", r"(Credit Card:|Credit Card is|My credit card is) (\d{4}-\d{4}-\d{4}-\d{4})")
 
+# Testing:
 
-text_with_pii = "John Doe's phone number is 03211234567."
-filtered_text = enigma_guard.filter(text_with_pii)
-original_text = enigma_guard.defilter(filtered_text)
-
-print(f"Original Text: {text_with_pii}")
-print(f"Filtered Text: {filtered_text}")
-print(f"Original Text: {original_text}")
+text = "My name is John Doe. My phone number is 923001234567. My email is faisal@gmail.com. My CNIC is 12345-1234567-1. My address is House 123, Street 123, Area 123, City 123. My passport is AB1234567. My credit card is 1234-5678-1234-5678."
+filtered_text = enigma_guard.filter(text)
+print(filtered_text)
+defiltered_text = enigma_guard.defilter(filtered_text)
+print(defiltered_text)
